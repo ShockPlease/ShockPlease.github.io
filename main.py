2602d391597716872acede68a1e2e9f0d78091cc
ShockPlease <118433139+ShockPlease@users.noreply.github.com>
2023-03-10 22:29:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ repo = 'ShockPlease.github.io'
 path = 'html/api/api.html'
 
 # Get the personal access token from an environment variable
-token = os.environ['my_token']
+token = os.environ.get("shock_token")
 # Retrieve the current contents of index.html
 headers = {
     'Authorization': f'token {token}',
